7893538f7d539e8558788ef4c50d201192d8ba4e
Flavio Pessoa <santf655@newschool.edu>
2017-11-03 22:05:06 +0000

    Exercise 10

---
+++ Assignment_10/Exercise_10.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+    app = express();
+const { Pool } = require('pg');
+
+// AWS RDS POSTGRESQL INSTANCE
+var db_credentials = new Object();
+db_credentials.user = 'mczoloft';
+db_credentials.host = config.AWSRDS_EP;
+db_credentials.database = 'sensorfp';
+db_credentials.password = config.AWSRDS_PW;
+db_credentials.port = 5432;
+
+app.get('/', function(req, res) {
+    // Connect to the AWS RDS Postgres database
+    const client = new Pool(db_credentials);
+
+    // SQL query
+    var q = `SELECT EXTRACT(DAY FROM sensortime AT TIME ZONE 'America/New_York') as sensorday, 
+             EXTRACT(MONTH FROM sensortime AT TIME ZONE 'America/New_York') as sensormonth,
+             EXTRACT(HOUR FROM sensortime AT TIME ZONE 'America/New_York') as sensorhour,
+             count(*) as num_obs, 
+             max(workMood) as max_work, 
+             min(workMood) as min_work,
+             max(workLight) as max_light, 
+             min(workLight) as min_light
+             FROM sensordata 
+             GROUP BY sensormonth, sensorday, sensorhour;`;
+             
+    client.connect();
+    client.query(q, (qerr, qres) => {
+        res.send(qres.rows);
+        console.log('responded to request');
+    });
+    client.end();
+});
+
+app.listen(3000, function() {
+    console.log('Server listening...');
+});
