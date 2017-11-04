ff471e611b8d09faee7eff20ae412a2d47ac8e3b
Flavio Pessoa <santf655@newschool.edu>
2017-11-04 20:07:40 +0000

    right variables

--- Assignment_10/Exercise_10.js
+++ Assignment_10/Exercise_10.js
@@ -17,15 +17,15 @@ app.get('/', function(req, res) {
     const client = new Pool(db_credentials);
 
     // SQL query
-    var q = `SELECT EXTRACT(DAY FROM realtime AT TIME ZONE 'America/New_York') as sensorday, 
-             EXTRACT(MONTH FROM realtime AT TIME ZONE 'America/New_York') as sensormonth,
-             EXTRACT(HOUR FROM realtime AT TIME ZONE 'America/New_York') as sensorhour,
+    var q = `SELECT EXTRACT(DAY FROM realTime AT TIME ZONE 'America/New_York') as sensorday, 
+             EXTRACT(MONTH FROM realTime AT TIME ZONE 'America/New_York') as sensormonth,
+             EXTRACT(HOUR FROM realTime AT TIME ZONE 'America/New_York') as sensorhour,
              count(*) as num_obs, 
              max(workMood) as max_work, 
              min(workMood) as min_work,
              max(workLight) as max_light, 
              min(workLight) as min_light
-             FROM studyroom 
+             FROM studyRoom 
              GROUP BY sensorday, sensorhour;`;
              
     client.connect();
