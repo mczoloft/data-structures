85f845ff801d18913adae681e379be385b112219
Flavio Pessoa <santf655@newschool.edu>
2017-12-10 22:33:20 +0000

    new path json

--- Teste2.js
+++ Teste2.js
@@ -12,16 +12,16 @@ console.log(db_credentials);
 // var url = process.env.ATLAS;
 
 // Retrieve
-// var MongoClient = require('mongodb').MongoClient; // npm install mongodb
+var MongoClient = require('mongodb').MongoClient; // npm install mongodb
 
-// MongoClient.connect(db_credentials.url, function(err, db) {
-//     if (err) {return console.dir(err);}
+MongoClient.connect(db_credentials, function(err, db) {
+    if (err) {return console.dir(err);}
 
-//     var collection = db.collection('meetings');
+    var collection = db.collection('meetings');
 
-//     // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
-//     collection.insert(meetings);
-//     db.close();
+    // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+    collection.insert(meetings);
+    db.close();
 
-// }); 
+}); 
 //MongoClient.connect
