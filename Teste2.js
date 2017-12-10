a77b5e519b4d11397d1e7680ae4db43fd52f692d
Flavio Pessoa <santf655@newschool.edu>
2017-12-10 22:31:01 +0000

    new path json

--- Teste2.js
+++ Teste2.js
@@ -1,28 +1,27 @@
 var fs = require('fs');
-var config = require('../sensitive_data/config');
+var config = require('../sensitive_data/config.js');
 
 //credentials stored securely
-var db_credentials = new Object();
-db_credentials.url = config.atlas.replace('"', '');
+var db_credentials = config.atlas;
 
 var meetings = JSON.parse(fs.readFileSync('FinalAssignment01/aaData.json'));
 
-// console.log(db_credentials.url);
+console.log(db_credentials);
 // Connection URL
 // var url = 'mongodb://' + process.env.IP + ':27017/aa';
 // var url = process.env.ATLAS;
 
 // Retrieve
-var MongoClient = require('mongodb').MongoClient; // npm install mongodb
+// var MongoClient = require('mongodb').MongoClient; // npm install mongodb
 
-MongoClient.connect(db_credentials.url, function(err, db) {
-    if (err) {return console.dir(err);}
+// MongoClient.connect(db_credentials.url, function(err, db) {
+//     if (err) {return console.dir(err);}
 
-    var collection = db.collection('meetings');
+//     var collection = db.collection('meetings');
 
-    // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
-    collection.insert(meetings);
-    db.close();
+//     // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
+//     collection.insert(meetings);
+//     db.close();
 
-}); 
+// }); 
 //MongoClient.connect
