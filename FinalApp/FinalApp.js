2349fab0c2af0311a000be505ab1443479bfbde7
Flavio Pessoa <santf655@newschool.edu>
2017-12-12 02:21:16 +0000

    new order

--- FinalApp/FinalApp.js
+++ FinalApp/FinalApp.js
@@ -31,9 +31,12 @@ app.get('/', function(req, res) {
     var q = `SELECT EXTRACT(DAY FROM realTime AT TIME ZONE 'America/New_York') as sensorday, 
              EXTRACT(MONTH FROM realTime AT TIME ZONE 'America/New_York') as sensormonth,
              EXTRACT(HOUR FROM realTime AT TIME ZONE 'America/New_York') as sensorhour,
-             (workMood) as work,
-             (workLight) as light
-             FROM studyRoom
+             count(*) as num_obs, 
+             max(workMood) as max_work, 
+             min(workMood) as min_work,
+             max(workLight) as max_light, 
+             min(workLight) as min_light
+             FROM studyRoom 
              GROUP BY realTime, sensorday, sensormonth, sensorhour;`;
              
     client.connect();
